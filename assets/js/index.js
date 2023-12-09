9b6bb28efd807b72d58230eb15bf0ff27bd26179
aleksiye <aleksiye@protonmail.com>
2023-12-09 22:53:02 +0100

    added view product functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,14 +2,14 @@
 // Not using it simplifes the project and reduces the time required to make it. Less JSON api calls means earlier completion and less debugging
 
 const storeItems = [
-    {id:"0", name: "Blue Mug with a Saucer",        price: 39.99,  category:"personal",  imgSm: "mug-saucer.avif"},
-    {id:"1", name: "Decorative Soviet Camera Lens", price: 91.12,  category:"decour",    imgSm: "camera-lens-old.avif"},
-    {id:"2", name: "Minimalist Molded Armchair",    price: 499.00, category:"furniture", imgSm: "chair-minimalist.avif"},
-    {id:"3", name: "Antique Typewriter",            price: 199.00, category:"decour",    imgSm: "typewriter-antique.avif"},
-    {id:"4", name: "Steel Cocktail Shaker",         price: 12.99,  category:"personal",  imgSm: "cup-steel.avif"},
-    {id:"5", name: "Coffee Mug with Wooden Base",   price: 19.99,  category:"personal",  imgSm: "mug-coffee.avif"},
-    {id:"6", name: "Jade Spinner",                  price: 75.99,  category:"decour",    imgSm: "spinner-gyro.avif"},
-    {id:"7", name: "Glass Dice",                    price: 9.99,   category:"misc",      imgSm: "dice.avif"},
+    {id:"0", name: "Blue Mug with a Saucer",        price: 39.99,  category:"personal",  img: "mug-saucer"          },
+    {id:"1", name: "Decorative Soviet Camera Lens", price: 91.12,  category:"decour",    img: "camera-lens"         },
+    {id:"2", name: "Minimalist Molded Armchair",    price: 499.00, category:"furniture", img: "chair-minimalist",   },
+    {id:"3", name: "Antique Typewriter",            price: 199.00, category:"decour",    img: "typewriter-antique"  },
+    {id:"4", name: "Steel Cocktail Shaker",         price: 12.99,  category:"personal",  img: "cup-steel"           },
+    {id:"5", name: "Coffee Mug with Wooden Base",   price: 19.99,  category:"personal",  img: "mug-coffee"          },
+    {id:"6", name: "Jade Spinner",                  price: 75.99,  category:"decour",    img: "spinner-gyro"        },
+    {id:"7", name: "Glass Dice",                    price: 9.99,   category:"misc",      img: "dice"                },
 ];
 
 function printStoreData(data){
@@ -19,18 +19,51 @@ function printStoreData(data){
         html += `<div id="product-${item.id}" class="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col" data-category="${item.category}" data-price="${item.price}">
         <button id="product-wrapper-${item.id}" class="product-wrapper relative" type="button">
         <div id="card-banner-${item.id}" class="hidden relative w-{0.5} flex justify-center left-{0.5} top-5 z-10 h-0"><p class="text-center">Add to cart</p></div>
-        <img class="hover:grow hover:shadow-lg" src="./assets/img/${item.imgSm}" data-purchaseable="true">
+        <img class="hover:grow hover:shadow-lg" src="./assets/img/${item.img}.avif" data-purchaseable="true">
         </button>
         <div class="pt-3 flex items-center justify-between">
             <p class="">${item.name}</p>
-            <svg class="h-6 w-6 fill-current text-gray-500 hover:text-black" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <path d="M12,4.595c-1.104-1.006-2.512-1.558-3.996-1.558c-1.578,0-3.072,0.623-4.213,1.758c-2.353,2.363-2.352,6.059,0.002,8.412 l7.332,7.332c0.17,0.299,0.498,0.492,0.875,0.492c0.322,0,0.609-0.163,0.792-0.409l7.415-7.415 c2.354-2.354,2.354-6.049-0.002-8.416c-1.137-1.131-2.631-1.754-4.209-1.754C14.513,3.037,13.104,3.589,12,4.595z M18.791,6.205 c1.563,1.571,1.564,4.025,0.002,5.588L12,18.586l-6.793-6.793C3.645,10.23,3.646,7.776,5.205,6.209 c0.76-0.756,1.754-1.172,2.799-1.172s2.035,0.416,2.789,1.17l0.5,0.5c0.391,0.391,1.023,0.391,1.414,0l0.5-0.5 C14.719,4.698,17.281,4.702,18.791,6.205z" />
-            </svg>
+            <button type="button" id="like-button-${item.id}" data-is-alternate="false"><svg class="h-6 w-6 fill-current text-gray-500 hover:text-black" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M12,4.595c-1.104-1.006-2.512-1.558-3.996-1.558c-1.578,0-3.072,0.623-4.213,1.758c-2.353,2.363-2.352,6.059,0.002,8.412 l7.332,7.332c0.17,0.299,0.498,0.492,0.875,0.492c0.322,0,0.609-0.163,0.792-0.409l7.415-7.415 c2.354-2.354,2.354-6.049-0.002-8.416c-1.137-1.131-2.631-1.754-4.209-1.754C14.513,3.037,13.104,3.589,12,4.595z M18.791,6.205 c1.563,1.571,1.564,4.025,0.002,5.588L12,18.586l-6.793-6.793C3.645,10.23,3.646,7.776,5.205,6.209 c0.76-0.756,1.754-1.172,2.799-1.172s2.035,0.416,2.789,1.17l0.5,0.5c0.391,0.391,1.023,0.391,1.414,0l0.5-0.5 C14.719,4.698,17.281,4.702,18.791,6.205z" />
+        </svg></button>
         </div>
-        <p class="relative pt-1 text-gray-900">${item.price}€ <a class="absolute right-0 bottom-0 text-sm inline-block no-underline border-b border-gray-600 leading-relaxed hover:text-black hover:border-black" href="#">view product</a> </p>
+        <p class="relative pt-1 text-gray-900">${item.price}€ <a id="view-product-${item.id}" class="absolute right-0 bottom-0 text-sm inline-block no-underline border-b border-gray-600 leading-relaxed hover:text-black hover:border-black" href="product.html">view product</a> </p>
     </div>`;
     }
     store.insertAdjacentHTML("beforeend",html);
+}
+function attachLikeEvent(data){
+    for(item of data){
+        $(`#like-button-${item.id}`).on("click",function (){
+            if(!$(this).data("is-alternate")){
+                $(this).html(`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+            <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+          </svg>
+          `);
+          $(this).data("is-alternate", true);
+          //Call like state change handler here, probably log it into database
+            }
+            else{
+                $(this).html(`<svg class="h-6 w-6 fill-current text-gray-500 hover:text-black" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path d="M12,4.595c-1.104-1.006-2.512-1.558-3.996-1.558c-1.578,0-3.072,0.623-4.213,1.758c-2.353,2.363-2.352,6.059,0.002,8.412 l7.332,7.332c0.17,0.299,0.498,0.492,0.875,0.492c0.322,0,0.609-0.163,0.792-0.409l7.415-7.415 c2.354-2.354,2.354-6.049-0.002-8.416c-1.137-1.131-2.631-1.754-4.209-1.754C14.513,3.037,13.104,3.589,12,4.595z M18.791,6.205 c1.563,1.571,1.564,4.025,0.002,5.588L12,18.586l-6.793-6.793C3.645,10.23,3.646,7.776,5.205,6.209 c0.76-0.756,1.754-1.172,2.799-1.172s2.035,0.416,2.789,1.17l0.5,0.5c0.391,0.391,1.023,0.391,1.414,0l0.5-0.5 C14.719,4.698,17.281,4.702,18.791,6.205z" />
+            </svg>`);
+            $(this).data("is-alternate", false);
+            //Call like state change handler here, probably log it into database
+            }
+        })
+    }
+}
+function viewProductHandler(event){
+    event.preventDefault();
+    localStorage.setItem("storeItem", JSON.stringify(storeItems[this.id.match(/\d+/)]));
+    window.open("../../product.html","_self");
+
+}
+function attachViewProductEvent(data){
+    for(item of data){
+        const element = document.getElementById(`view-product-${item.id}`);
+        element.addEventListener('click',viewProductHandler);
+    }
 }
 
 function increaseTotal(ammount){
@@ -85,7 +118,7 @@ function addToCart(id){
     document.getElementById("cart-content").insertAdjacentHTML("beforeend",`
     <li id="cart-product-${item.id}"class="flex py-6">
     <div class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-      <img src="./assets/img/${item.imgSm}" alt="${item.name}" class="h-full w-full object-cover object-center">
+      <img src="./assets/img/${item.img}.avif" alt="${item.name}" class="h-full w-full object-cover object-center">
     </div>
 
     <div class="ml-4 flex flex-1 flex-col">
@@ -113,9 +146,9 @@ function addToCart(id){
     </div>
   </li>`);
     increaseTotal(item.price);
-    $(`#increase-item-${item.id}`).click(increaseQuantity);
-    $(`#decrease-item-${item.id}`).click(decreaseQuantity);
-    $(`#remove-item-${item.id}`).click(function(){
+    $(`#increase-item-${item.id}`).on("click",increaseQuantity);
+    $(`#decrease-item-${item.id}`).on("click",decreaseQuantity);
+    $(`#remove-item-${item.id}`).on("click",function(){
         removeItem(this);
     });
     successfullyAdded(item.id);
@@ -133,12 +166,13 @@ function storeItemMouseOutHandler (){
 
 window.addEventListener('load',function(){
     printStoreData(storeItems);
-    
+    attachLikeEvent(storeItems);
+    attachViewProductEvent(storeItems);
     $(`.product-wrapper`).on( "mouseenter", storeItemMouseInHandler).on( "mouseleave", storeItemMouseOutHandler);
-    $('.product-wrapper').click(function(){
+    $('.product-wrapper').on("click",function(){
         addToCart(this.id.slice(-1));
     })
-    $(".cart-toggle").click(function(){
+    $(".cart-toggle").on("click", function(){
         $('#cart-wrapper').toggleClass("opacity-0 opacity-100 ");
         $('#cart-wrapper').children().toggleClass("end-full inset-0");
         $($('#cart-wrapper').children()[1]).children().toggleClass("end-full inset-0");
